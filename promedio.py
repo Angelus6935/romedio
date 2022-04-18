d4793d505bc69d79ce2cf397f0dcca12bc24f3db
Angelus6935 <japontehuenei@gmail.com>
2022-04-18 16:58:26 -0300

    se agreda version 1 de promedio

---
+++ promedio.py
@@ -0,0 +1,11 @@
+n = int(input("numeros a promediar:"))
+suma=0
+i=1
+while (i <= n):
+	print("ingrese numero",i)
+	nota = float(input())
+	suma=suma+nota
+	i+=1
+prom = suma / n 
+print("El promedio:",prom)
+	
