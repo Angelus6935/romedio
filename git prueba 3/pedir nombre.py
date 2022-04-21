3671e923b544192e8be957432a38647a29250f03
Angelus6935 <japontehuenei@gmail.com>
2022-04-21 10:12:20 -0300

    prueba 3

---
+++ git prueba 3/pedir nombre.py
@@ -0,0 +1,15 @@
+import time
+
+nombre = input("Ingrese nombre: ")
+
+while nombre =="":
+    print("¡Error de ingreso!")
+    nombre = input("Ingrese nombre: ")
+
+print("hola"+" "+nombre)
+
+#time.sleep(2)
+
+exit(5)
+
+    
